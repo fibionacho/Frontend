274de2923f343541d95d0dbec7a5baa74b123228
fibionacho <nabifo@hotmail.com>
2022-10-03 18:35:15 +0200

    radio

---
+++ radio circunferencia/radio.js
@@ -0,0 +1,4 @@
+const Pi=Math.PI;
+let radio= prompt("Introduce el radio de la circunferencia");
+let area=(radio*radio)*Pi;
+document.write(`El area del ciruculo de radio${radio} es igual a ${area}`);
