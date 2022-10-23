fb9df1ac4e0e0d6d1288ee2ab5b6b76ce7a08587
fibionacho <nabifo@hotmail.com>
2022-10-23 11:52:44 +0200

    ej3

---
+++ UT03/ej3.js
@@ -0,0 +1,15 @@
+/**Realizar una funcion que reciba como aurgumento un array con
+ * dos elementos de tipo cadena y devuelva true si en el primer elemento
+ * de la matriz se encuentran todas las letras del segundo elemento
+ * de la matriz.
+ */
+
+
+
+let arr=["javascript", "java"];
+
+function compara(ar) {
+    for(i=0;i<=ar[0].length;i++){
+        
+    }
+}
