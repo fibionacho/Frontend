9f7dcb55bd2061a71990d06b8384ea9037693e96
fibionacho <nabifo@hotmail.com>
2022-12-02 14:08:03 +0100

    5.4

--- ejercicio edades/edad.js
+++ ejercicio edades/edad.js
@@ -1,7 +1,7 @@
  edad1=prompt("Introduce la primera edad");
  edad2=prompt("Introduce la segunda edad");
  edad3=prompt("Introduce la tercera edad");
-edad4=prompt("Introduce la cuarta edad");
+ edad4=prompt("Introduce la cuarta edad");
 
 let arrayedad=[edad1,edad2,edad3,edad4];
 
@@ -11,7 +11,5 @@ function sumaProducto(){
     let operacion=(parseInt(arrayedad[0])+parseInt(arrayedad[3]))+(parseInt(arrayedad[1])*parseInt(arrayedad[2]));
     return operacion;
 }
-
-
 sumaProducto(arrayedad);
 document.write(`el resultado es ${sumaProducto(arrayedad)}`);
