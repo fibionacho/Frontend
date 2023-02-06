043a371bd568aa75fd5b663d3bd52c02dee86f9c
fibionacho <nabifo@hotmail.com>
2023-02-06 19:24:07 +0100

    consulta AJAX

---
+++ ejercicio 7/7.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json');
+require("db.php");
+
+if (isset($_POST)) {
+    $name = $_POST["nombre"];
+    $consulta = $db->prepare("SELECT Nombre from alumnos WHERE Nombre = :nombre LIMIT 1");
+    $consulta->execute([":nombre" => $name]);
+    $user = $consulta->fetch();
+
+    if ($user["Nombre"] == $name) {
+        echo json_encode("Este nombre ya ha sido elegido ");
+    } else {
+        echo json_encode("Este nombre todavia no ha sido registrado");
+    }
+}
+
+?>
