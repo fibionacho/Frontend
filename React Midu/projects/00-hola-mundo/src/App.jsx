83db64dbd95dd5c5b5a9ba2874fb4a0f18046cb8
fibionacho <nabifo@hotmail.com>
2023-06-22 17:53:25 +0200

    primer proyecto React

---
+++ React Midu/projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+export function App (){
+    return(
+        <>
+            <TwitterFollowCard userName="mrdanas" name="Danielo Alchino"/>
+            <TwitterFollowCard userName="gersinki" name="Jefferson Ataque"/>
+            <TwitterFollowCard userName="pabel_4" name="Pablo MediaOreja"/>
+        </>
+    )
+}
