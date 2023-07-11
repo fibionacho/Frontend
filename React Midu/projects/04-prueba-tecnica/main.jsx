09edb12689da977f033306c29acc672c02e2d06b
fibionacho <nabifo@hotmail.com>
2023-07-11 12:33:09 +0200

    prueba técnica finished

--- React Midu/projects/04-prueba-tecnica/main.jsx
+++ React Midu/projects/04-prueba-tecnica/main.jsx
@@ -2,4 +2,4 @@ import { createRoot } from 'react-dom/client'
 import { App } from './src/App'
 
 const root = createRoot(document.getElementById('app'))
-root.render(<App/>)
+root.render(<App />)
--- React Midu/projects/04-prueba-tecnica/src/App.jsx
+++ React Midu/projects/04-prueba-tecnica/src/App.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
 export function App () {
-    return (
-        <h1>App de gatitos</h1>
-    )
+  const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
+  const END_POINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
+
+  // efecto para recuperar el dato
+  useEffect(() => {
+    fetch(END_POINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+      })
+  }, [])
+
+  // efecto para cargar la imagen relacionada
+  useEffect(() => {
+    const firstWord = fact.split(' ', 3).join(' ')
+    console.log(firstWord)
+    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { url } = response
+        setImageUrl(`https://cataas.com/${url}`)
+      })
+  }, [])
+
+  return (
+    <main>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt='cat' />}
+    </main>
+  )
 }
