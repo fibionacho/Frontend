903e7deee6f8b045b66aa5b1b47ea814367843a4
fibionacho <nabifo@hotmail.com>
2023-07-12 11:40:41 +0200

    custom hook

--- React Midu/projects/04-prueba-tecnica/src/App.jsx
+++ React Midu/projects/04-prueba-tecnica/src/App.jsx
@@ -1,24 +1,12 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { getRandomFact } from './services/facts'
 
-export function App () {
-  const [fact, setFact] = useState()
+function useCatImage ({ fact }) {
   const [imageUrl, setImageUrl] = useState()
-  const END_POINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-
-
-  // efecto para recuperar el dato
-  useEffect(() => {
-    fetch(END_POINT_RANDOM_FACT)
-      .then(res => res.json())
-      .then(data => {
-        const { fact } = data
-        setFact(fact)
-      })
-  }, [])
-
   // efecto para cargar la imagen relacionada
   useEffect(() => {
+    if (!fact) return
     const firstWord = fact.split(' ', 3).join(' ')
     console.log(firstWord)
     fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
@@ -27,10 +15,25 @@ export function App () {
         const { url } = response
         setImageUrl(`https://cataas.com/${url}`)
       })
+  }, [fact])
+  return { imageUrl }
+}
+export function App () {
+  const [fact, setFact] = useState()
+  const { imageUrl } = useCatImage({ fact })
+  // efecto para recuperar el dato
+  useEffect(() => {
+    getRandomFact().then(setFact)
   }, [])
+
+  const handeClick = async () => {
+    const newFact = await getRandomFact()
+    setFact(newFact)
+  }
 
   return (
     <main>
+      <button onClick={handeClick}>Nuevo dato</button>
       {fact && <p>{fact}</p>}
       {imageUrl && <img src={imageUrl} alt='cat' />}
     </main>
---
+++ React Midu/projects/04-prueba-tecnica/src/services/facts.js
@@ -0,0 +1,8 @@
+const END_POINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+
+export const getRandomFact = async () => {
+  const res = await fetch(END_POINT_RANDOM_FACT)
+  const data = await res.json()
+  const { fact } = data
+  return fact
+}
