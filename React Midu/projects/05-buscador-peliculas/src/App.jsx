1986b765da168c108fff0f894c1186af5845ab4a
fibionacho <nabifo@hotmail.com>
2023-07-20 18:35:50 +0200

    inir form

--- React Midu/projects/05-buscador-peliculas/src/App.jsx
+++ React Midu/projects/05-buscador-peliculas/src/App.jsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
@@ -6,14 +7,26 @@ import { useMovies } from './hooks/useMovies'
 
 function App() {
   const { movies: mappedMovies } = useMovies()
+  const inputRef = useRef()
+  const [query, setQuery] = useState('')
+
+  const handleSubmit = (event) =>{
+    event.preventDefault()
+    console.log(query)
+  }
+
+  const handleChange = (event)=>{
+    setQuery(event.target.value)
+  }
+  
   return (
     <div className='page'>
       <header>
         <h1>Prueba técnica</h1>
 
-        <form className='form'>
-          <input placeholder='Busca tu pelicula'/>
-          <button type='submit'>Buscar</button>
+        <form className='form' onSubmit={handleSubmit}>
+          <input onChange={handleChange} name='query' value={query} ref={ inputRef } placeholder='Busca tu pelicula'/>
+          <button  type='submit'>Buscar</button>
         </form>
       </header>
 
