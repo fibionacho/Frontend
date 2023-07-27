e6a1a7f1bbbcf615b02036e174f0cefefc6b85ac
fibionacho <nabifo@hotmail.com>
2023-07-27 12:31:55 +0200

    estilado grid

--- React Midu/projects/05-buscador-peliculas/src/App.jsx
+++ React Midu/projects/05-buscador-peliculas/src/App.jsx
@@ -1,33 +1,63 @@
-import { useRef, useState } from 'react'
+import { useEffect,  useState, useRef } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
+function useSearch(){
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+  const isFirstInput = useRef('true')
 
+  useEffect(()=>{
+    if(isFirstInput.current){
+      isFirstInput.current = search == ''
+      return
+    }
+    if(search==''){
+      setError('No se puede buyscar una película vacía')
+      return
+    }
+
+    if(search.match(/^\d+$/)){
+      setError('No se puede buscar una película con un número')
+      return
+    }
+
+    if(search.length<3){
+      setError('La búsqueda debe de tener al menos 3 caracteres')
+      return
+    }
+    setError(null)
+  }, [search])
+  return { search, updateSearch, error }
+}
 
 function App() {
   const { movies: mappedMovies } = useMovies()
-  const inputRef = useRef()
-  const [query, setQuery] = useState('')
+  const { search, updateSearch, error } = useSearch()
+
+
 
   const handleSubmit = (event) =>{
     event.preventDefault()
-    console.log(query)
+    console.log(search)
   }
 
   const handleChange = (event)=>{
-    setQuery(event.target.value)
+    updateSearch(event.target.value)
   }
-  
+
+
   return (
     <div className='page'>
       <header>
         <h1>Prueba técnica</h1>
 
         <form className='form' onSubmit={handleSubmit}>
-          <input onChange={handleChange} name='query' value={query} ref={ inputRef } placeholder='Busca tu pelicula'/>
+          <input onChange={handleChange} name='query' value={search} placeholder='Busca tu pelicula'/>
           <button  type='submit'>Buscar</button>
         </form>
+        {error && <p className='error'>{error}</p>}
       </header>
 
       <main>
